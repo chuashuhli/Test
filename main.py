581f4ae2785b87a54037135b3f2e06a544cc736d
chuashuhli <chuashuhli@gmail.com>
2024-11-02 20:47:59 +0800

    Test commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from logics.customer_query_handler import process_user_message
+from pydantic import BaseModel
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="My Streamlit App"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Streamlit App")
+
+form = st.form(key="form")
+form.subheader("Prompt")
+
+user_prompt = form.text_area("Enter your prompt here", height=200)
+
+if form.form_submit_button("Submit"):
+    st.toast(f"User Input Submitted - {user_prompt}")
+    response = process_user_message(user_prompt) #<--- This calls the `process_user_message` function that we have created 🆕
+    st.write(response)
+    print(f"User Input is {user_prompt}")
